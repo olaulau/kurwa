1b99f2a3348eeb65f93a8c8e75d069079622b842
olaulau <olaulau@gmail.com>
2015-08-17 02:15:25 +0200

    - small correction to preload in firefox - traffic control notes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(function() {
 	var video = $('video#kurwa_video');
+	var video_ = video.get(0);
+	
 	video.hide();
+	video_.play(); // needed for Firefox to start loading the video
 	
 	video.bind("loadeddata",function(e){
+//		console.log("loadeddata");
 		videoDuration = video.prop('duration');
 		bufferCompleted = false;
 		
@@ -11,8 +15,8 @@ $(function() {
 			var percent = 100 * buffered / videoDuration;
 //			console.log(percent + " %");
 			if(!bufferCompleted && percent >= 100) {
-				bufferCompleted = true; // trigger completed only once even if we continue to receive progess events
-//				alert("buffering completed !");
+				bufferCompleted = true; // trigger "buffering completed" only once even if we continue to receive progress events
+//				console.log("buffering completed !");
 				video.show(500);
 				video.get(0).play();
 			}
